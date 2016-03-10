6049998a2f9d07b95e14ec7ca41d7f513554e48d
thecoons <antobarth@gmail.com>
2016-03-10 23:18:04 +0100

    maj Datasummary et UML

--- graal-keyval/src/main/java/fr/lirmm/graphik/graal/keyval/DataSummary.java
+++ graal-keyval/src/main/java/fr/lirmm/graphik/graal/keyval/DataSummary.java
@@ -1,5 +1,18 @@
 package fr.lirmm.graphik.graal.keyval;
 
-public class DataSummary {
+import fr.lirmm.graphik.util.Profilable;
+import fr.lirmm.graphik.util.Profiler;
+
+public class DataSummary implements Profilable{
+
+	public Profiler getProfiler() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setProfiler(Profiler arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
